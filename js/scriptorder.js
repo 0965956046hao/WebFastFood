c8e11384b43e390db7abbf38fd3ec195d7ea7a08
Xuan Giang <xuangiang9a1np@gmail.com>
2023-03-21 09:56:52 +0700

    FE-Update-21/02-Gaing

---
+++ js/scriptorder.js
@@ -0,0 +1,63 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+}
+
+let account = document.querySelector('.user-account');
+
+document.querySelector('#user-btn').onclick = () => {
+    account.classList.add('active');
+}
+
+
+let myOrders = document.querySelector('.my-orders');
+
+document.querySelector('#order-btn').onclick = () => {
+    myOrders.classList.add('active');
+}
+
+document.querySelector('#close-orders').onclick = () => {
+    myOrders.classList.remove('active');
+}
+
+let heartt = document.querySelector('.heart');
+
+document.querySelector('#heart-btn').onclick = () => {
+    heartt.classList.add('active');
+    console.log('aaa');
+}
+
+document.querySelector('#close-heart').onclick = () => {
+    heartt.classList.remove('active');
+}
+
+window.onscroll = () => {
+    myOrders.classList.remove('active');
+    cart.classList.remove('active');
+    heartt.classList.remove('active');
+};
+
+let slides = document.querySelectorAll('.home-bg .home .slide-container .slide');
+let index = 0;
+
+function next() {
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev() {
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+
+let accordion = document.querySelectorAll('.faq .accordion-container .accordion');
+
+accordion.forEach(acco => {
+    acco.onclick = () => {
+        accordion.forEach(remove => remove.classList.remove('active'));
+        acco.classList.add('active');
+    }
+});
