a2529826bd42e6a8f9856926f865dd2ff8e7d81b
San Võ <Sanvo1120@gmail.com>
2023-03-31 16:38:38 +0700

    san gui 3

---
+++ FastfoodWeb/routes/accounts.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var ItemModel = require('../controllers/accountCtrl');
+var ShowResult = require('../configs/errorHandle');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', async function(req, res, next) {
+    try {
+        var list = await ItemModel.listItem();
+        ShowResult.returnResult(res, 200, true, list);
+    } catch (error) {
+        ShowResult.returnResult(res, 400, false, error);
+    }
+});
+
+router.get('/:id', async function(req, res, next) {
+    let ID = req.params.id;
+    try {
+        var list = await ItemModel.GetItemById(ID);
+        ShowResult.returnResult(res, 200, true, list);
+    } catch (error) {
+        ShowResult.returnResult(res, 400, false, error);
+    }
+});
+
+router.post('/add', async function(req, res, next) {
+    try {
+        console.log(req.body)
+        var list = await ItemModel.AddAnItem(req.body);
+        ShowResult.returnResult(res, 200, true, list);
+    } catch (error) {
+        ShowResult.returnResult(res, 400, false, error);
+    }
+
+});
+
+router.put('/edit/:id', async function(req, res, next) {
+    let params = {};
+    params.id = req.params.id;
+    params.update = req.body;
+    var list = await ItemModel.UpdateAnItem(params);
+    ShowResult.returnResult(res, 200, true, list);
+});
+
+
+router.delete('/delete/:id', async function(req, res, next) {
+    try {
+        var list = await ItemModel.DeleteAnItem(req.params.id);
+        ShowResult.returnResult(res, 200, true, list);
+    } catch (error) {
+        ShowResult.returnResult(res, 400, false, error);
+    }
+});
+
+module.exports = router;
